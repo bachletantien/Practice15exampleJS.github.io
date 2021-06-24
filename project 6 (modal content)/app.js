37f912491e9bb144bd02c9702aaf234656cf7af3
Tan Tien <mrbanhu147@gmail.com>
2021-06-24 18:37:00 +0700

    upload app.js project 6 and completed

--- project 6 (modal content)/app.js
+++ project 6 (modal content)/app.js
@@ -1,1 +1,18 @@
-//select
+//select modal-btn . modal-overlay , close-btnW
+
+//listen for click event on modal-btn and close-btn
+
+//when user clicks modal-btn add .OPEN-MODAL to modal-overlay
+
+
+const modalBtn = document.querySelector('.modal-btn');
+const modal = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
+
+modalBtn.addEventListener('click' , function(){
+    modal.classList.add("open-modal");
+});
+
+closeBtn.addEventListener('click' , function(){
+    modal.classList.remove("open-modal");
+});
