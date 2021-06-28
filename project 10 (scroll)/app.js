3226d0e4ae98cb10d849b3371fb41fe19182506d
Tan Tien <mrbanhu147@gmail.com>
2021-06-28 21:45:58 +0700

    update fixed navbar

--- project 10 (scroll)/app.js
+++ project 10 (scroll)/app.js
@@ -1,14 +1,18 @@
 
 //Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewports.
+//pageOffset is a read-only window property that returns the number of pixels the document has been scrolled vertically
+//slice extracts a section of a string without modifying orginal string
+//offsetTop - A number, representing the top position of the element, in pixels
 
 
 
 // ***************set date ********************
+//select span
 const date = document.getElementById('date');
-
 date.innerHTML = new Date().getFullYear();
-// ************ close links *********
 
+
+// ************ close links *********
 const navToggle = document.querySelector('.nav-toggle');
 const linksContainer = document.querySelector('.links-container');
 const links = document.querySelector('.links');
@@ -27,7 +31,34 @@ navToggle.addEventListener('click', function(){
 
 
 
-
 // ************ fixed navbar *********
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+
+window.addEventListener("scroll" , function() {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+
+    if(scrollHeight > navHeight){
+        navbar.classList.add("fixed-nav");
+    }else{
+        navbar.classList.remove("fixed-nav");
+    }
+
+    // setup back to top links
+    if (scrollHeight > 500) {
+    console.log("helo");
+
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
+
+
+// *********** smooth scroll ***************
+
+
 
 
